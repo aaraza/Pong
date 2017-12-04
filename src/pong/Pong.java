8544d36d4b41a2a161806b8a8b453c5a34da8db3
Ali Raza <aardz6@mail.missouri.edu>
2017-12-03 18:53:22 -0600

    Added main class and style sheet

---
+++ src/pong/Pong.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pong;
+
+import javafx.application.Application;
+import static javafx.application.Application.STYLESHEET_MODENA;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Ali
+ */
+public class Pong extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        
+        HBox root = new HBox();
+        root.setPrefSize(600, 400);
+
+        Scene scene = new Scene(root);
+       
+        Switchable.scene = scene;
+        Switchable.switchController("MainMenu");
+        
+        stage.setResizable(false);
+        stage.setTitle("Pong");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */ 
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
